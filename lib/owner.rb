d26e670bd87c0d8cb811b5f328f2c3b41f66f66a
Joseph Sesti <sestijoey63@gmail.com>
2019-09-19 17:10:30 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def buy_cat(name)
+    
 end
