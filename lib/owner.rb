86190731a7068f6a31bc3673423b1a6468c71f0a
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 00:38:13 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,4 +25,40 @@ class Owner
   def self.reset_all
     @@all.clear
   end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat
+    #can buy a cat that is an instance in Cat Class
+    #knows about its cats
+  end
+  
+  def buy_dog
+    #same for dog
+  end
+  
+  def walk_dogs
+    #mood happy
+  end
+  
+  def feed_cats
+    #mood happy
+  end
+  
+  def sell_pets
+    #can sell all pets
+    #mood nervous
+    #leaves them without owner
+  end
+  
+  def list_pets
+    #list all pets
+  end
+  
 end
